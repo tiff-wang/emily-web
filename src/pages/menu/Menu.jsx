dbc32a69750f08e194200ef47fbf0f1872136d00
tiff-wang <wangtiffany@gmail.com>
2019-08-13 18:35:13 -0400

    menu page done

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import 'pages/menu/Menu.scss'
 
 class Menu extends Component {
     render() {
         return (
-            <div>
-                <h2>Menu</h2>
+            <div className='Menu'>
+                <div className='menu-placeholder center'>
+                    <h1> Menu placeholder </h1>
+                </div>
             </div>
         );
     }
