32ccf210febbb71ed990d4332c360f795ee0a117
tiff-wang <wangtiffany@gmail.com>
2019-08-13 20:46:45 -0400

    finished shop

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,10 +1,72 @@
 import React, { Component } from 'react'
+import { Row, Col, Container} from 'reactstrap';
+
+import 'pages/shop/Shop.scss'
+import placeholder from 'assets/images/shop-placeholder.jpg'
 
 class Shop extends Component {
     render() {
         return (
-            <div>
-                <h2>Shop</h2>
+            <div className='Shop'>
+                <Container>
+                    <Row>
+                        <Col md='4'>
+                            <div className='card'>
+                                <a>
+                                    <div className='card-image'>
+                                        <img src={placeholder}/>
+                                    </div>
+                                    <div className='card-description'>
+                                        <p> Item name </p>
+                                        <p> $0.00 </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </Col>
+
+                        <Col md='4'>
+                            <div className='card'>
+                                <a>
+                                    <div className='card-image'>
+                                        <img src={placeholder}/>
+                                    </div>
+                                    <div className='card-description'>
+                                        <p> Item name </p>
+                                        <p> $0.00 </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </Col>
+
+                        <Col md='4'>
+                            <div className='card'>
+                                <a>
+                                    <div className='card-image'>
+                                        <img src={placeholder}/>
+                                    </div>
+                                    <div className='card-description'>
+                                        <p> Item name </p>
+                                        <p> $0.00 </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </Col>
+
+                        <Col md='4'>
+                            <div className='card'>
+                                <a>
+                                    <div className='card-image'>
+                                        <img src={placeholder}/>
+                                    </div>
+                                    <div className='card-description'>
+                                        <p> Item name </p>
+                                        <p> $0.00 </p>
+                                    </div>
+                                </a>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
